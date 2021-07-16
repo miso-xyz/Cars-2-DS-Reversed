ea08dbe890f6b3ca3e0bec83d7962a2a1a430c2b
miso.kiyu <61099345+miso-xyz@users.noreply.github.com>
2021-07-16 05:52:02 +0100

    Add files via upload

---
+++ Scripts/surfaces/surfaceNames.luac_decompiled.lua
@@ -0,0 +1,21 @@
+local L0_1, L1_1, L2_1, L3_1, L4_1, L5_1, L6_1, L7_1, L8_1, L9_1
+L0_1 = {}
+L1_1 = "Tarmac"
+L2_1 = "Magic"
+L3_1 = "Grass"
+L4_1 = "Dirt"
+L5_1 = "Gravel"
+L6_1 = "Sand"
+L7_1 = "WetTarmac"
+L8_1 = "Blossom"
+L9_1 = "Equalizer"
+L0_1[1] = L1_1
+L0_1[2] = L2_1
+L0_1[3] = L3_1
+L0_1[4] = L4_1
+L0_1[5] = L5_1
+L0_1[6] = L6_1
+L0_1[7] = L7_1
+L0_1[8] = L8_1
+L0_1[9] = L9_1
+surfaceDebugNames = L0_1
